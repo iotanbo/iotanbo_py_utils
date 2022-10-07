d69153843c38e78d33f517152d6215e2aa6abc28
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 04:44:18 +0000

    Adding tarfile member sanitization to extractall()

--- src/iotanbo_py_utils/file_utils.py
+++ src/iotanbo_py_utils/file_utils.py
@@ -1420,7 +1420,26 @@ def extract_gzip_archive_ne(
             )
     try:
         with tarfile.open(src, "r:*") as t:
-            t.extractall(dest)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, dest)
         if remove_src:
             remove_src_result = remove_file_ne(src)
             if remove_src_result.is_err():
